2c460ef709dfef56cb86717a0b3e70376a562d35
Steven Le <stevenleusa79@yahoo.com>
2023-06-01 08:44:23 -0700

    finished ExperienceComponent draft, need to fill in data

--- src/Components/Experience/Components/ExperienceCard.js
+++ src/Components/Experience/Components/ExperienceCard.js
@@ -1,12 +1,17 @@
-import React from "react";
-import "./ExperienceCard.css";
+import { Card, CardImg, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import './ExperienceCard.css';
 
-function ExperienceCard({ frontContent, backContent }) {
-    return (
-      <div className="experience-card">
-        
+function ExperienceCard({ photo, title, dates, description }) {
+  return (
+    <Card className="experience-card">
+      <CardImg className="experience-image" top width="100%" src={photo} alt="Experience Photo" />
+      <div className="card-content">
+        <CardTitle tag="h3" className="title">{title}</CardTitle>
+        <CardSubtitle tag="h4" className="subtitle">{dates}</CardSubtitle>
+        <CardText className="description">{description}</CardText>
       </div>
-    );
-  }
+    </Card>
+  );
+}
 
 export default ExperienceCard;
--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -3,22 +3,71 @@ import "./Experience.css";
 import ExperienceCard from "./Components/ExperienceCard";
 
 function Experience() {
+  const experienceData = [
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+    {
+      photo:'https://github.com/steeevin88/steeevin88/assets/59713070/077d2dc3-0e0a-49a4-bd8c-444013b07738',
+      title:'Instructional Student Assistant',
+      dates:'May 2023',
+      description:'alskdjfk;asjflaskfjlas'
+    },
+  ];
+
   return (
     <div className="experience-page" id="experience">
-        <div className='experience-table'> 
-        <ExperienceCard
-            frontContent={<div>Card 1 - Front</div>}
-            backContent={<div>Card 1 - Back</div>}
-        />
-        <ExperienceCard
-            frontContent={<div>Card 2 - Front</div>}
-            backContent={<div>Card 2 - Back</div>}
-        />
-        <ExperienceCard
-            frontContent={<div>Card 3 - Front</div>}
-            backContent={<div>Card 3 - Back</div>}
-        />
-        </div>
+      <div className="experience-header">
+          <b>MY EXPERIENCES</b>
+      </div>
+      <div className='experience-table'>
+      {experienceData.map((experience, index) => {
+        return (
+          <div className="experience-container" key={index}>
+            <ExperienceCard {...experience} />
+          </div>
+        );
+      })}
+      </div>
     </div>
   );
 }
